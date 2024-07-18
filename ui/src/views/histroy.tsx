e441eac9ec65fc6b8a5d1f687ac77e8f29a3ba80
liaojs <272781702@qq.com>
2024-07-18 11:50:42 +0800

    chore: update ui

--- ui/src/views/histroy.tsx
+++ ui/src/views/histroy.tsx
@@ -1,16 +1,15 @@
-import { defineComponent, onMounted, reactive } from "vue"
-import { dateFormat } from 'js-hodgepodge'
-import { ElMessage } from 'element-plus'
+import { defineComponent, onMounted, reactive } from 'vue';
+import { dateFormat } from 'js-hodgepodge';
+import { ElMessage } from 'element-plus';
 
-import { getHistroys, download, getPublishList, publish } from '../api'
-import { download as dow } from '../utils/download'
-import LogDialog from "./logDialog"
-import PublishDialog from "./publishDialog"
-import { PublishItem } from "./publishConfig"
+import { getHistroys, download, getPublishList, publish } from '../api';
+import { download as dow } from '../utils/download';
+import LogDialog from './logDialog';
+import PublishDialog from './publishDialog';
+import { PublishItem } from './publishConfig';
 
 export default defineComponent({
   setup() {
-
     const state = reactive({
       historys: [],
       logDialogVisible: false,
@@ -19,174 +18,214 @@ export default defineComponent({
       collapse: [],
       loading: false,
       publishList: [],
-      publishId: ''
+      publishId: '',
     } as {
       historys: {
-        buildTime: number,
-        projectName: string,
-        status: string,
-        branch: string
-      }[],
-      logDialogVisible: boolean,
-      publishDialogVisible: boolean,
-      currentName: string,
-      collapse: string[],
-      loading: boolean,
-      publishList: PublishItem[],
-      publishId: string
-    })
+        buildTime: number;
+        projectName: string;
+        status: string;
+        branch: string;
+      }[];
+      logDialogVisible: boolean;
+      publishDialogVisible: boolean;
+      currentName: string;
+      collapse: string[];
+      loading: boolean;
+      publishList: PublishItem[];
+      publishId: string;
+    });
 
     const handler = {
-
       getPublishList: () => {
-        getPublishList<[]>()
-          .then(res => {
-            state.publishList = res.data.content || []
-          })
+        getPublishList<[]>().then((res) => {
+          state.publishList = res.data.content || [];
+        });
       },
 
-      handleChange: () => {
-
-      },
+      handleChange: () => {},
 
       viewLog: (name: string) => {
-        state.logDialogVisible = true
-        state.currentName = name
+        state.logDialogVisible = true;
+        state.currentName = name;
       },
 
       publish: (name: string) => {
-        state.currentName = name
+        state.loading = true
+        
+        state.currentName = name;
 
-        const {
-          id,
-          describe,
-          ...right
-        } = state.publishList.find((c: PublishItem) => c.id === state.publishId) || {} as PublishItem
-
+        const { id, describe, ...right } =
+          state.publishList.find(
+            (c: PublishItem) => c.id === state.publishId,
+          ) || ({} as PublishItem);
+        
         publish({
           ...right,
-          projectName: name
+          projectName: name,
         })
-          .then(res => {
-            ElMessage({ message: 'success!!', type: 'success' })
-            state.loading = false
-
+          .then((res) => {
+            ElMessage({ message: 'success!!', type: 'success' });
+            state.loading = false;
           })
-          .catch(() => (state.loading = false))
+          .catch(() => (state.loading = false));
       },
 
-      download: (row: { buildTime: number, projectName: string, status: string }) => {
+      download: (row: {
+        buildTime: number;
+        projectName: string;
+        status: string;
+      }) => {
         if (row.status === 'error') {
           return ElMessage({
             type: 'error',
-            message: 'error build!!!'
-          })
+            message: 'error build!!!',
+          });
         }
-        state.loading = true
-        download<any>(
-          `${row.projectName}-${row.buildTime}`
-        )
-          .then(res => {
+        state.loading = true;
+        download<any>(`${row.projectName}-${row.buildTime}`)
+          .then((res) => {
             console.log(res);
-
 
             dow.export({
               res,
-              beforeDownload: () => state.loading = true,
-              afterDownload: () => state.loading = false,
-              error: () => state.loading = false
-            })
+              beforeDownload: () => (state.loading = true),
+              afterDownload: () => (state.loading = false),
+              error: () => (state.loading = false),
+            });
           })
-          .catch(() => state.loading = false)
-      }
-
-    }
+          .catch(() => (state.loading = false));
+      },
+    };
 
     onMounted(() => {
-      state.loading = true
-      getHistroys<[{
-        buildTime: number,
-        projectName: string,
-        status: string,
-        branch: string
-      }]>()
-        .then(res => {
-          state.historys = (res.data.content || []).sort((a, b) => b.buildTime - a.buildTime)
+      state.loading = true;
+      getHistroys<
+        [
+          {
+            buildTime: number;
+            projectName: string;
+            status: string;
+            branch: string;
+          },
+        ]
+      >()
+        .then((res) => {
+          state.historys = (res.data.content || []).sort(
+            (a, b) => b.buildTime - a.buildTime,
+          );
 
-          state.collapse = state.historys.map(c => `${c.buildTime} - ${c.projectName}`)
+          state.collapse = state.historys.map(
+            (c) => `${c.buildTime} - ${c.projectName}`,
+          );
 
-          state.loading = false
+          state.loading = false;
         })
-        .catch(() => state.loading = false)
-    })
+        .catch(() => (state.loading = false));
+    });
 
-    return () => <div v-loading={state.loading}>
-      <el-collapse onChange={handler.handleChange} v-model={state.collapse}>
-        {
-          state.historys.map(c => (
+    return () => (
+      <div v-loading={state.loading}>
+        <el-collapse onChange={handler.handleChange} v-model={state.collapse}>
+          {state.historys.map((c) => (
             <el-collapse-item
               v-slots={{
-                title: () => <div>
-                  {dateFormat(c.buildTime, '{Y}-{MM}-{DD} {A} {hh}:{ii}:{ss}')} - {c.projectName}
-                </div>
+                title: () => (
+                  <div>
+                    {dateFormat(
+                      c.buildTime,
+                      '{Y}-{MM}-{DD} {A} {hh}:{ii}:{ss}',
+                    )}{' '}
+                    - {c.projectName}
+                  </div>
+                ),
               }}
-              name={`${c.buildTime} - ${c.projectName}`}>
+              name={`${c.buildTime} - ${c.projectName}`}
+            >
               <el-descriptions title={c.projectName}>
-                <el-descriptions-item label="Build Time">{dateFormat(c.buildTime, '{Y}-{MM}-{DD} {A} {hh}:{ii}:{ss}')}</el-descriptions-item>
+                <el-descriptions-item label="Build Time">
+                  {dateFormat(c.buildTime, '{Y}-{MM}-{DD} {A} {hh}:{ii}:{ss}')}
+                </el-descriptions-item>
                 <el-descriptions-item label="result">
-                  <el-tag size="small" type={c.status === 'success' ? 'success' : 'danger'}>{c.status}</el-tag>
+                  <el-tag
+                    size="small"
+                    type={c.status === 'success' ? 'success' : 'danger'}
+                  >
+                    {c.status}
+                  </el-tag>
                 </el-descriptions-item>
                 <el-descriptions-item label="">
-                  <el-button type="primary" onClick={() => handler.viewLog(`${c.projectName}-${c.buildTime}`)}>log</el-button>
+                  <el-button
+                    type="primary"
+                    onClick={() =>
+                      handler.viewLog(`${c.projectName}-${c.buildTime}`)
+                    }
+                  >
+                    log
+                  </el-button>
                 </el-descriptions-item>
-                <el-descriptions-item label="Branch"><span style={{ fontWeight: 'bold' }}>{c.branch}</span></el-descriptions-item>
+                <el-descriptions-item label="Branch">
+                  <span style={{ fontWeight: 'bold' }}>{c.branch}</span>
+                </el-descriptions-item>
                 <el-descriptions-item label="product">
-                  <el-link type="primary" onClick={() => handler.download(c)}>download</el-link>
+                  <el-link type="primary" onClick={() => handler.download(c)}>
+                    download
+                  </el-link>
                 </el-descriptions-item>
-                {
-                  c.status === 'success' && (
-                    <el-descriptions-item label="">
-                      <el-popover trigger="click" placement="top" width="300" v-slots={{
-                        reference: () => <el-button type="primary" onClick={handler.getPublishList}>publish</el-button>
-                      }}>
-                        <p>Are you sure to publish this?</p>
-                        <el-select
-                          v-model={state.publishId}
-                          filterable
-                          clearable
-                        >
-                          {
-                            state.publishList.map((item: PublishItem) => (
-                              <el-option key={item.id} label={item.describe} value={item.id}></el-option>
-                            ))
-                          }
-                        </el-select>
-                        <div style="text-align: right; margin-top: 20px">
-                          <el-button size="small" type="primary" onClick={() => handler.publish(`${c.projectName}-${c.buildTime}`)} >
-                            confirm
+                <el-descriptions-item label="">
+                  {c.status === 'success' && (
+                    <el-popover
+                      trigger="click"
+                      placement="top"
+                      width="300"
+                      v-slots={{
+                        reference: () => (
+                          <el-button
+                            type="primary"
+                            onClick={handler.getPublishList}
+                          >
+                            publish
                           </el-button>
-                        </div>
-                      </el-popover>
-                    </el-descriptions-item>
-                  )
-                }
+                        ),
+                      }}
+                    >
+                      <p>Are you sure to publish this?</p>
+                      <el-select v-model={state.publishId} filterable clearable>
+                        {state.publishList.map((item: PublishItem) => (
+                          <el-option
+                            key={item.id}
+                            label={item.describe}
+                            value={item.id}
+                          ></el-option>
+                        ))}
+                      </el-select>
+                      <div style="text-align: right; margin-top: 20px">
+                        <el-button
+                          size="small"
+                          type="primary"
+                          onClick={() =>
+                            handler.publish(`${c.projectName}-${c.buildTime}`)
+                          }
+                        >
+                          confirm
+                        </el-button>
+                      </div>
+                    </el-popover>
+                  )}
+                </el-descriptions-item>
               </el-descriptions>
             </el-collapse-item>
-          ))
-        }
-      </el-collapse>
+          ))}
+        </el-collapse>
 
-      {
-        state.logDialogVisible && (
+        {state.logDialogVisible && (
           <LogDialog
             projectName={state.currentName}
             dialogFormVisible={state.logDialogVisible}
-            onCloseDialog={() => state.logDialogVisible = false}
+            onCloseDialog={() => (state.logDialogVisible = false)}
           />
-        )
-      }
+        )}
 
-      {/* {
+        {/* {
         state.publishDialogVisible && (
           <PublishDialog
             projectName={state.currentName}
@@ -195,6 +234,7 @@ export default defineComponent({
           />
         )
       } */}
-    </div>
-  }
-})
+      </div>
+    );
+  },
+});
